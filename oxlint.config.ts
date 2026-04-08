355dc56dad5235e731c05df47e2a3b29ca26e062
Oleg Michailov <source.code@michailov.lt>
2026-04-08 17:56:27 +0300

    Add verify script

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'oxlint';
 import { typescript, config } from '@rivuty/oxlint-config';
+import { defineConfig } from 'oxlint';
 
 export default defineConfig({
   extends: [typescript, config],
